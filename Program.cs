465b2982a61a73b71ec2427fa4d680c2aef637f0
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2025-03-05 09:43:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[14];
+        int Count = 0; 
+        Console.WriteLine("Введите 14 целых чисел:");
+
+        for (int i = 0; i < 14; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+
+            if (array[i] % 2 == 0) 
+                Count++;
+        }
+
+        Console.WriteLine("\nВведённый массив:");
+        Console.WriteLine(string.Join(" ", array));
+
+        
+        Console.WriteLine($"Количество чётных элементов: {Count}");
+        Console.ReadLine();
+    }
+}
+
